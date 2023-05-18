4e8e1ad5089f6c784168d687cade06c96775c8fd
Guilherme Moreno <111452588+Gui250@users.noreply.github.com>
2023-05-18 08:19:34 -0300

    Add files via upload

---
+++ calculadora/calculadora.rb
@@ -0,0 +1,13 @@
+print "Digite um número: "
+
+numero = gets.chomp.to_i 
+
+print "Digite um segundo número: "
+
+numero2 = gets.chomp.to_i 
+
+adition = numero + numero2 
+
+puts "A soma dos números é #{adition}"
+
+
